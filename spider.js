318f9a07c49197357a5a94532b70e5cba12df14f
Ravi-naya <Nayakravinayak005@gmail.com>
2021-07-25 13:50:05 +0530

    chanages

---
+++ spider.js
@@ -0,0 +1,9 @@
+spiderJson = {"nodes":[
+{"weight":2,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":0.0, "id":3, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":8.569970425351725, "id":4, "url":"http://www.dr-chuck.com/dr-chuck/resume/bio.htm"},
+{"weight":1,"rank":9.034985212675862, "id":11, "url":"http://www.dr-chuck.com/dr-chuck/resume/pictures/index.htm"}],
+"links":[
+{"source":0,"target":2,"value":3},
+{"source":0,"target":3,"value":3},
+{"source":2,"target":0,"value":3}]};
